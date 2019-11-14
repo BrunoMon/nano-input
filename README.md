e0abbcec615ca8257e94aac64b44a232cc7c4d61
Bruno Monfrinotti <brunomon@hotmail.com>
2019-11-14 15:43:34 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,143 @@
+
+# @BrunoMon/nano-input
+
+![npm](https://img.shields.io/npm/v/@brunomon/odata-fetch-factory?style=flat-square)
+![GitHub issues](https://img.shields.io/github/issues/BrunoMon/odata-fetch-factory)
+![GitHub](https://img.shields.io/github/license/brunomon/odata-fetch-factory)
+
+lit-element input
+
+## Install
+
+```
+$ npm install @brunomon/nano-input
+```
+
+## Usage
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <meta name="mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+    <title>Prueba</title>
+</head>
+<style>
+    html {
+        height: 100vh;
+        width: 100vw;
+        margin: 0;
+        padding: 0;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 2.5vmin;
+
+        --primary-color: #7b1fa2;
+        --primary-color-variant: #ae52d4;
+
+        --secondary-color: #e64a19;
+        --secondary-color-variant: #ff7d47;
+
+        --background-color: #ffffff;
+        --surface-color: #ffffff;
+        --error-color: #B00020;
+
+        --onprimary-color: #ffffff;
+        --onprimary-color-variant: #000000;
+
+        --onsecondary-color: #000000;
+        --onsecondary-color-variant: #ffffff;
+
+        --onbackground-color: #000000;
+
+        --onsurface-color: rgba(0, 0, 0, .87);
+        --onsurface-color-helper: rgba(0, 0, 0, .60);
+        --onsurface-color-disable: rgba(0, 0, 0, .38);
+
+        --onerror-color: #ffffff;
+    }
+
+    body {
+        padding: 1rem;
+        height: 100vh;
+        width: 100vw;
+        background-color: rgba(0, 0, 0, .3);
+
+
+    }
+
+    .card {
+        width: 50vmin;
+        display: grid;
+        grid-gap: 2rem;
+        background-color: var(--background-color);
+        padding: 1rem
+    }
+
+    nano-input {
+        background-color: rgba(0, 0, 0, .1);
+        color: var(--onsurface-color);
+        border-top-left-radius: .3rem;
+        border-top-right-radius: .3rem;
+
+    }
+
+    nano-input[type="button"] {
+        width: 8rem;
+        border-radius: .5rem;
+        overflow: hidden
+    }
+</style>
+
+<body>
+    <div class="card">
+        <nano-input label="Texto" helper="* requerido" id="input1"></nano-input>
+        <nano-input label="Numero" helper="* entre 10 y 20" type="number" value="2" id="input2"></nano-input>
+        <nano-input label="Fecha Hora" type="datetime-local" id="input3"></nano-input>
+        <nano-input label="Fecha" type="date"></nano-input>
+        <nano-input label="Password" type="password"></nano-input>
+        <nano-input label="E-mail" type="email"></nano-input>
+        <nano-input label="Este es el checkbox" type="checkbox" id="inputCheck"></nano-input>
+        <nano-input label="VALIDAR" type="button" value="" id="boton"></nano-input>
+    </div>
+
+
+</body>
+<script>
+    document.getElementById("input1").addEventListener("validating", (e) => {
+        e.currentTarget.errorText = !e.currentTarget.empty ? "" : "* No debe ser vacio"
+    })
+
+    document.getElementById("input2").addEventListener("validating", (e) => {
+        const valor = parseInt(e.currentTarget.value)
+        e.currentTarget.errorText = (valor >= 10 && valor <= 20) ? "" : "* debe estar entre 10 y 20"
+    })
+
+    document.getElementById("inputCheck").leadingIcon = null
+
+    document.getElementById("boton").onclick = validate
+
+    function validate() {
+        let isValid = Array.prototype.slice.call(document.querySelectorAll("nano-input")).reduce((isValid, item) => {
+            item.validate()
+            isValid = isValid && item.isValid
+            return isValid
+        }, true)
+        if (isValid) {
+            alert("OK")
+        }
+
+    }
+</script>
+
+</html>
+```
---
+++ index.js
@@ -0,0 +1,296 @@
+import {
+    html,
+    css,
+    LitElement
+} from "lit-element"
+
+export class nanoInput extends LitElement {
+    constructor() {
+        super()
+        this.type = "text"
+        this.readonly = false
+        this.disabled = false
+        this.label = ""
+        this._value = ""
+        this.empty = true
+        this.errorText = ""
+        this.isValid = true
+        this.leadingIcon = null
+        this.addEventListener("click", (e) => {
+            this.focus()
+        })
+        this.addEventListener("touch", (e) => {
+            this.focus()
+        })
+    }
+    focus(e) {
+        if (!this.readonly) this.shadowRoot.querySelector("#input").focus()
+    }
+    static get styles() {
+        return css `
+        :host
+        {
+            position:relative;
+            display:grid;
+            grid-template-columns:auto 1fr;
+            border-bottom:1px solid var(--onsurface-color-helper);
+            overflow-y:visible;
+            min-height:3rem;
+        }
+        :host([type="button"]){
+            border-bottom:none;
+        }
+        svg{
+            align-self:center;
+            justify-self:center;
+            padding:.5rem;
+            fill:var(--onsurface-color-helper);
+            stroke:var(--onsurface-color-helper)
+        }
+        #lead{
+            display:grid
+        }
+      
+        #container{
+            position:relative;
+            display:grid
+        }
+        :host([disabled]) #input,
+        :host([disabled]) svg,
+        :host([disabled]) #helper,
+        :host([disabled]) #label{
+            color:var( --onsurface-color-disable);
+            fill:var(--onsurface-color-disable);
+            stroke:var(--onsurface-color-disable)
+        }
+        #input{
+            position:absolute;
+            color:inherit;
+            bottom:0;
+            border:none;
+            width:95%;
+            height:2rem;
+            outline:none;
+            background-color:transparent;
+            font-family:inherit;
+            font-size:inherit;
+            left:.5rem;
+
+        }
+        label #label {
+            pointer-events:none;
+            position:absolute;
+            display:grid;
+            left:.5rem;
+            margin:0;
+            width:100%;
+            height:100%;
+            z-index:1;
+            top:calc(100% - 2rem);
+            transition:all .3s ease;
+            color:var(--primary-color);
+        }
+        :host([type="button"]) label #label{
+            transition:none;
+            width:auto;
+        }
+        :host([type="button"])  #input{
+            color:var(--onsurface-color-helper);
+            font-size:.8rem;
+        }
+
+        :host(:not([type="checkbox"]):not([type="button"])) #input:focus + label #label, 
+        :host(:not([empty])) #input + label #label,
+        #input[type="date"] + label #label,
+        #input[type="datetime-local"] + label #label{
+            top:.4rem;
+            font-size:.8rem
+        }
+        
+        #input:focus + label #border{
+            width:100%
+        }
+        :host([haveleading]) #input:focus + label #border{
+            width:calc(100% + 2.5rem);
+        }
+        :host([haveleading]) #border{
+            left:-2.5rem;
+        }
+        #border{
+            clear:both;
+            pointer-events:none;
+            position:absolute;
+            left:0;
+            margin:0;
+            width:0;
+            height:auto;
+            z-index:2;
+            bottom:0;
+            transition:all .3s ease;
+            border-bottom:2px solid var(--primary-color);
+        }
+        :host([type="button"]) #border{
+            height:100%;
+            background: linear-gradient(30deg,  transparent 0%, var(--primary-color) 200%);
+            border-bottom:none;
+
+        }
+        #helper,#error{
+            position:absolute;
+            font-size:.7rem;
+            top:100%;
+            padding:.3rem
+        }
+        #helper{
+            color:var(--onsurface-color-variant)
+        }
+        #error,:host(:not([is-valid])) #label{
+            color:var(--error-color)
+        }
+        :host(:not([is-valid])) #helper{
+            display:none
+        }
+        
+        :host([is-valid]) #error{
+            display:none
+        }
+
+        :host([type="checkbox"]) #input{
+            opacity:0;
+            width:1.5rem;
+            height:1.5rem;
+            justify-self:end
+        }
+        :host([type="checkbox"]) #input + label::after,:host([type="checkbox"]) #input + label::before{
+            position:absolute;
+            right:.5rem;
+            top:50%;
+            transform:translateY(-50%);
+            content:"";
+            width:1rem;
+            height:1rem;
+            border:2px solid;
+            border-radius:.2rem;
+            color:inherit;
+            text-align:center;
+            line-height: 1rem;
+        }
+        :host([type="checkbox"]) #input:checked + label::before{
+            content:"✔";
+            color:var(--primary-color)
+        }
+        
+        `
+    }
+
+    render() {
+        return html `
+            <div id="lead">
+                ${this.leadingIcon}
+            </div>
+            <div id="container">
+                <input id="input" .value="${this.value}" spellcheck="false" autocomplete="off" type="${this.type}" ?readonly=${this.readonly} ?disabled=${this.disabled} @change="${(e)=> this.value=e.currentTarget.value}" @blur="${this.blur}">
+                <label for="input">
+                    <div id="label">${this.label}</div>
+                    <div id="border"></div>
+                </label>
+                <div id="helper">${this.helper}</div>
+                <div id="error">${this.errorText}</div>
+            </div>
+           
+            `
+
+    }
+    static get properties() {
+        return {
+            label: {
+                type: String,
+                reflect: true
+            },
+            type: {
+                type: String,
+                reflect: true
+            },
+            readonly: {
+                type: Boolean,
+                reflect: true
+            },
+            helper: {
+                type: String,
+                reflect: true
+            },
+            isValid: {
+                type: Boolean,
+                attribute: "is-valid",
+                reflect: true
+            },
+            empty: {
+                type: Boolean,
+                reflect: true
+            },
+            disabled: {
+                type: Boolean,
+                reflect: true
+            },
+            value: {
+                type: String,
+                reflect: true
+            },
+            errorText: {
+                type: String,
+                reflect: true
+            },
+            leadingIcon: {
+                type: Object
+            },
+            haveLeading: {
+                type: Boolean,
+                reflect: true
+            },
+            disabled: {
+                type: Boolean,
+                reflect: true
+            }
+        }
+
+    }
+    set leadingIcon(value) {
+        const oldValue = this._leadingIcon;
+        this._leadingIcon = value
+        this.haveLeading = (this._leadingIcon != null)
+        this.requestUpdate('leadingIcon', oldValue);
+    }
+    get leadingIcon() {
+        return this._leadingIcon
+    }
+    set errorText(value) {
+        const oldValue = this._errorText;
+        this._errorText = value
+        this.isValid = (this._errorText == "")
+        this.requestUpdate('errorText', oldValue);
+    }
+    get errorText() {
+        return this._errorText
+    }
+    set value(value) {
+        const oldValue = this._value;
+        this._value = value
+        this.empty = (this._value == "")
+        this.requestUpdate('value', oldValue);
+        const ev = new Event("validating")
+        this.dispatchEvent(ev)
+    }
+    get value() {
+        return this._value
+    }
+    blur(e) {
+        const ev = new Event("validating")
+        this.dispatchEvent(ev)
+    }
+    validate() {
+        const ev = new Event("validating")
+        this.dispatchEvent(ev)
+    }
+
+}
+window.customElements.define('nano-input', nanoInput);
